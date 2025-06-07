6347d11cfe8c6771f91fc0b3ac71cc36813459ac
yanditv <yankur44@gmail.com>
2025-06-07 10:09:42 -0500

    se migro la base de datos

---
+++ gad_checa_gestion_cementerio/Migrations/20250607150516_persona_propietario_relation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gad_checa_gestion_cementerio.Migrations
+{
+    /// <inheritdoc />
+    public partial class persona_propietario_relation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Persona_NumeroIdentificacion",
+                table: "Persona",
+                column: "NumeroIdentificacion",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Persona_NumeroIdentificacion",
+                table: "Persona");
+        }
+    }
+}
